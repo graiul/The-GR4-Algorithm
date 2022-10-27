c82baf857b7c36da2736298331fc8aed602868d9
Gheorghica Radu <radugheorghica@protonmail.com>
2022-10-27 10:30:40 +0300

    Am sters unele linii comentate si am comentat o varianta veche a metodei send_to_kingdom.

--- GR4_Algorithm.py
+++ GR4_Algorithm.py
@@ -11,170 +11,171 @@ class GR4_Algorithm(object):
         self.circuit = quantum_circuit
         self.circuit_matrix = None
 
-    def send_to_kingdom(self, quantum_backend_name, circuit_object):
-        # IBMQ.save_account('e6bbad0f51ab787aec48bed242c422777f1680f0428e19b34e19bbcd467a2faff2bdcedd0cbb04b19f4bb700f66900728f778d4bc8226785e6c35dc818374ac8',
-        #                   overwrite=True)
-        provider = IBMQ.load_account()
-        # print(IBMQ.providers())
-        # print(IBMQ.get_provider(hub='ibm-q', group='open', project='main'))
+    # Aceasta varianta a acestei metode este cea din GR3.
+    # def send_to_kingdom(self, quantum_backend_name, circuit_object):
+    #     # IBMQ.save_account('e6bbad0f51ab787aec48bed242c422777f1680f0428e19b34e19bbcd467a2faff2bdcedd0cbb04b19f4bb700f66900728f778d4bc8226785e6c35dc818374ac8',
+    #     #                   overwrite=True)
+    #     provider = IBMQ.load_account()
+    #     # print(IBMQ.providers())
+    #     # print(IBMQ.get_provider(hub='ibm-q', group='open', project='main'))
+    #
+    #     # backend = simulator = provider.get_backend('ibmq_qasm_simulator')
+    #     # backend = simulator = provider.get_backend('ibmq_lima')
+    #     backend = provider.get_backend(quantum_backend_name)
+    #
+    #     # # CIRCUITS
+    #
+    #     # #
+    #     # https://qiskit.org/documentation/getting_started.html
+    #     # # Create a Quantum Circuit acting on the q register
+    #     # circuit = QuantumCircuit(2, 2)
+    #     # # Add a H gate on qubit 0
+    #     # circuit.h(0)
+    #     # # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+    #     # circuit.cx(0, 1)
+    #     # # Map the quantum measurement to the classical bits
+    #     # circuit.measure([0,1], [0,1])
+    #     # #
+    #
+    #     # #
+    #     # https://quantum-computing.ibm.com/composer/docs/guide/grovers-algorithm
+    #     # Grover pentru 00
+    #     # qreg_q = QuantumRegister(2, 'q')
+    #     # creg_c = ClassicalRegister(2, 'c')
+    #     # circuit = QuantumCircuit(qreg_q, creg_c)
+    #     # circuit.reset(qreg_q[0])
+    #     # circuit.reset(qreg_q[1])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.h(qreg_q[0])
+    #     # circuit.s(qreg_q[1])
+    #     # circuit.s(qreg_q[0])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.cx(qreg_q[0], qreg_q[1])
+    #     # circuit.s(qreg_q[0])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.h(qreg_q[0])
+    #     # circuit.s(qreg_q[1])
+    #     # circuit.x(qreg_q[0])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.x(qreg_q[1])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.cx(qreg_q[0], qreg_q[1])
+    #     # circuit.x(qreg_q[0])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.h(qreg_q[0])
+    #     # circuit.x(qreg_q[1])
+    #     # circuit.h(qreg_q[1])
+    #     # circuit.measure(qreg_q[0], creg_c[0])
+    #     # circuit.measure(qreg_q[1], creg_c[1])
+    #
+    #     # Test nr 1, rulat neparalel in 23 MAR 2021
+    #     # si paralel in 24 MAR 2021
+    #
+    #     # VECHI
+    #     # https://qiskit.org/textbook/ch-algorithms/grover.html
+    #     # circuit = self.initialize_s(self.prepare_quantum_circuit(2), [0, 1])
+    #     # VECHI
+    #
+    #     # Execute the circuit on the qasm simulator
+    #     job = execute(circuit_object, backend, shots=8192)
+    #     # print(backend.name())
+    #
+    #     # #
+    #     # https://medium.com/analytics-vidhya/grovers-algorithm-in-python-c1dfa132e3af
+    #     #
+    #     from qiskit.tools.monitor import job_monitor
+    #     job_monitor(job, interval=2)
+    #     # #
+    #
+    #     # Grab results from the job
+    #     result = job.result()
+    #
+    #     # Returns counts
+    #     counts = result.get_counts(circuit_object)
+    #     # print("\nTotal count for 00 and 11 are:",counts)
+    #     # print("\nTotal count for 00, 01, 10 and 11 are:", counts)
+    #     print("\nCounts: ", counts)
 
-        # backend = simulator = provider.get_backend('ibmq_qasm_simulator')
-        # backend = simulator = provider.get_backend('ibmq_lima')
-        backend = provider.get_backend(quantum_backend_name)
-
-        # # CIRCUITS
-
-        # #
-        # https://qiskit.org/documentation/getting_started.html
-        # # Create a Quantum Circuit acting on the q register
-        # circuit = QuantumCircuit(2, 2)
-        # # Add a H gate on qubit 0
-        # circuit.h(0)
-        # # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-        # circuit.cx(0, 1)
-        # # Map the quantum measurement to the classical bits
-        # circuit.measure([0,1], [0,1])
-        # #
-
-        # #
-        # https://quantum-computing.ibm.com/composer/docs/guide/grovers-algorithm
-        # Grover pentru 00
-        # qreg_q = QuantumRegister(2, 'q')
-        # creg_c = ClassicalRegister(2, 'c')
-        # circuit = QuantumCircuit(qreg_q, creg_c)
-        # circuit.reset(qreg_q[0])
-        # circuit.reset(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.s(qreg_q[1])
-        # circuit.s(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.cx(qreg_q[0], qreg_q[1])
-        # circuit.s(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.s(qreg_q[1])
-        # circuit.x(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.x(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.cx(qreg_q[0], qreg_q[1])
-        # circuit.x(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.x(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.measure(qreg_q[0], creg_c[0])
-        # circuit.measure(qreg_q[1], creg_c[1])
-
-        # Test nr 1, rulat neparalel in 23 MAR 2021
-        # si paralel in 24 MAR 2021
-
-        # VECHI
-        # https://qiskit.org/textbook/ch-algorithms/grover.html
-        # circuit = self.initialize_s(self.prepare_quantum_circuit(2), [0, 1])
-        # VECHI
-
-        # Execute the circuit on the qasm simulator
-        job = execute(circuit_object, backend, shots=8192)
-        # print(backend.name())
-
-        # #
-        # https://medium.com/analytics-vidhya/grovers-algorithm-in-python-c1dfa132e3af
-        #
-        from qiskit.tools.monitor import job_monitor
-        job_monitor(job, interval=2)
-        # #
-
-        # Grab results from the job
-        result = job.result()
-
-        # Returns counts
-        counts = result.get_counts(circuit_object)
-        # print("\nTotal count for 00 and 11 are:",counts)
-        # print("\nTotal count for 00, 01, 10 and 11 are:", counts)
-        print("\nCounts: ", counts)
-
-# https://quantumcomputing.stackexchange.com/questions/13667/qiskit-get-gates-from-circuit-object
-
-# for gate in circuit.data:
-    # print('\ngate name:', gate[0].name)
-    # print('qubit(s) acted on:', gate[1])
-    # print('other paramters (such as angles):', gate[0].params)
-    # print(gate)
-
-# nr_of_qubits = circuit_6_3.num_qubits
-# print(nr_of_qubits)
-
-# send_to_kingdom('ibmq_lima', circuit_6_3)
-
-# https://www.geeksforgeeks.org/number-of-ways-to-split-n-as-sum-of-k-numbers-from-the-given-range/
-# Python3 implementation to count the
-# number of ways to divide N in K
-# groups such that each group
-# has elements in range [L, R]
-# mod = 1000000007
-#
-# # DP Table
-# dp = [[-1 for j in range(1000)]
-#       for i in range(1000)]
-#
-# # Function to count the number
-# # of ways to divide the number N
-# # in K groups such that each group
-# # has number of elements in range [L, R]
-# def calculate(pos, left, k, L, R):
-#
-#     # Base Case
-#     if (pos == k):
-#         if (left == 0):
-#             return 1
-#         else:
-#             return 0
-#
-#     # if N is divides completely
-#     # into less than k groups
-#     if (left == 0):
-#         return 0
-#
-#     # If the subproblem has been
-#     # solved, use the value
-#     if (dp[pos][left] != -1):
-#         return dp[pos][left]
-#
-#     answer = 0
-#
-#     # put all possible values
-#     # greater equal to prev
-#     for i in range(L, R + 1):
-#         if (i > left):
-#             break
-#
-#         answer = (answer +
-#                   calculate(pos + 1,
-#                             left - i,
-#                             k, L, R)) % mod
-#
-#     dp[pos][left] = answer
-#
-#     return answer
-#
-# # Function to count the number of
-# # ways to divide the number N
-# def countWaystoDivide(n, k, L, R):
-#
-#     return calculate(0, n, k, L, R)
-
-# Driver code
-# if __name__ == "__main__":
-#     N = 12
-#     K = 3
-#     L = 1
-#     R = 5
-#
-#     print(countWaystoDivide(N, K, L, R))
-
-# This code is contributed by rutvik_56
+    # https://quantumcomputing.stackexchange.com/questions/13667/qiskit-get-gates-from-circuit-object
+    #
+    # for gate in circuit.data:
+    #     print('\ngate name:', gate[0].name)
+    #     print('qubit(s) acted on:', gate[1])
+    #     print('other paramters (such as angles):', gate[0].params)
+    #     print(gate)
+    #
+    # nr_of_qubits = circuit_6_3.num_qubits
+    # print(nr_of_qubits)
+    #
+    # send_to_kingdom('ibmq_lima', circuit_6_3)
+    #
+    # https://www.geeksforgeeks.org/number-of-ways-to-split-n-as-sum-of-k-numbers-from-the-given-range/
+    # Python3 implementation to count the
+    # number of ways to divide N in K
+    # groups such that each group
+    # has elements in range [L, R]
+    # mod = 1000000007
+    #
+    # # DP Table
+    # dp = [[-1 for j in range(1000)]
+    #       for i in range(1000)]
+    #
+    # # Function to count the number
+    # # of ways to divide the number N
+    # # in K groups such that each group
+    # # has number of elements in range [L, R]
+    # def calculate(pos, left, k, L, R):
+    #
+    #     # Base Case
+    #     if (pos == k):
+    #         if (left == 0):
+    #             return 1
+    #         else:
+    #             return 0
+    #
+    #     # if N is divides completely
+    #     # into less than k groups
+    #     if (left == 0):
+    #         return 0
+    #
+    #     # If the subproblem has been
+    #     # solved, use the value
+    #     if (dp[pos][left] != -1):
+    #         return dp[pos][left]
+    #
+    #     answer = 0
+    #
+    #     # put all possible values
+    #     # greater equal to prev
+    #     for i in range(L, R + 1):
+    #         if (i > left):
+    #             break
+    #
+    #         answer = (answer +
+    #                   calculate(pos + 1,
+    #                             left - i,
+    #                             k, L, R)) % mod
+    #
+    #     dp[pos][left] = answer
+    #
+    #     return answer
+    #
+    # # Function to count the number of
+    # # ways to divide the number N
+    # def countWaystoDivide(n, k, L, R):
+    #
+    #     return calculate(0, n, k, L, R)
+    #
+    # Driver code
+    # if __name__ == "__main__":
+    #     N = 12
+    #     K = 3
+    #     L = 1
+    #     R = 5
+    #
+    #     print(countWaystoDivide(N, K, L, R))
+    #
+    # This code is contributed by rutvik_56
 
     # Aceasta e prima metoda care trebuie apelata in programul principal ca si operatie asupra unui circuit creat.
     # Fiecare linie a matricii reprezinta un qubit, iar elementele unei linii reprezinta portile de pe qubitul respectiv.
@@ -231,7 +232,6 @@ class GR4_Algorithm(object):
     # Acest numar este numarul de qubiti de care dispune calculatorul/simulatorul cuantic selectat.
     # Acest numar poate fi obtinut printr-o comanda qiskit:
     # https://quantumcomputing.stackexchange.com/questions/17375/is-there-any-way-to-obtain-the-number-of-qubits-of-a-given-backend-in-qiskit
-
     def quantum_circuit_matrix_part_getter(self, nr_of_qubits_per_part):
         from dask.distributed import Lock
         lock = Lock()
@@ -356,6 +356,7 @@ class GR4_Algorithm(object):
     #     print("-------------------------------")
 
     # Va fi apelat pe fiecare proces Dask
+
     def send_to_kingdom(self, quantum_backend_name, circuit_object):
         # IBMQ.save_account('e6bbad0f51ab787aec48bed242c422777f1680f0428e19b34e19bbcd467a2faff2bdcedd0cbb04b19f4bb700f66900728f778d4bc8226785e6c35dc818374ac8',
         #                   overwrite=True)
@@ -366,63 +367,6 @@ class GR4_Algorithm(object):
         # backend = simulator = provider.get_backend('ibmq_qasm_simulator')
         # backend = simulator = provider.get_backend('ibmq_lima')
         backend = provider.get_backend(quantum_backend_name)
-
-        # # CIRCUITS
-
-        # #
-        # https://qiskit.org/documentation/getting_started.html
-        # # Create a Quantum Circuit acting on the q register
-        # circuit = QuantumCircuit(2, 2)
-        # # Add a H gate on qubit 0
-        # circuit.h(0)
-        # # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-        # circuit.cx(0, 1)
-        # # Map the quantum measurement to the classical bits
-        # circuit.measure([0,1], [0,1])
-        # #
-
-        # #
-        # https://quantum-computing.ibm.com/composer/docs/guide/grovers-algorithm
-        # Grover pentru 00
-        # qreg_q = QuantumRegister(2, 'q')
-        # creg_c = ClassicalRegister(2, 'c')
-        # circuit = QuantumCircuit(qreg_q, creg_c)
-        # circuit.reset(qreg_q[0])
-        # circuit.reset(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.s(qreg_q[1])
-        # circuit.s(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.cx(qreg_q[0], qreg_q[1])
-        # circuit.s(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.s(qreg_q[1])
-        # circuit.x(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.x(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.cx(qreg_q[0], qreg_q[1])
-        # circuit.x(qreg_q[0])
-        # circuit.h(qreg_q[1])
-        # circuit.h(qreg_q[0])
-        # circuit.x(qreg_q[1])
-        # circuit.h(qreg_q[1])
-        # circuit.measure(qreg_q[0], creg_c[0])
-        # circuit.measure(qreg_q[1], creg_c[1])
-
-        # Test nr 1, rulat neparalel in 23 MAR 2021
-        # si paralel in 24 MAR 2021
-
-
-
-
-
-        # VECHI
-        # https://qiskit.org/textbook/ch-algorithms/grover.html
-        # circuit = self.initialize_s(self.prepare_quantum_circuit(2), [0, 1])
-        # VECHI
 
         # Execute the circuit on the qasm simulator
         job = execute(circuit_object, backend, shots=8192)
